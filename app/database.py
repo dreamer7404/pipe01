409ba15149520aadb2bea65c2a407acb0171775b
안경수 <dreamer7404@daum.net>
2025-05-28 23:15:34 +0900

    alembic

--- app/database.py
+++ app/database.py
@@ -2,11 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./myapi.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./pipe01.db"
 
+# 데이터베이스 연결설정
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engin);
-
+# 세션설정
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+session = SessionLocal()
 Base = declarative_base()
